187611c5eaf3c0e50271204fd19846e41e657c22
bglee <deptno@gmail.com>
2019-10-18 19:12:10 +0900

    refactor: 디렉토리 구조에 맞춰서 `describe` 뎁스 조정

--- __tests__/unit/api/comment.spec.ts
+++ __tests__/unit/api/comment.spec.ts
@@ -1,52 +1,55 @@
 import {createApi} from '../../../src/api'
 import {DynamoDB, S3} from 'aws-sdk'
-import {EValidationErrorMessage} from '../../../src/entity/error'
 import {Post} from '../../../src/entity/post'
 
-describe('api', () => {
-  describe('comment', () => {
-    const api = createApi({
-      ddbClient: new DynamoDB.DocumentClient({region: 'ap-northeast-2'}),
-      s3Client: new S3({region: 'ap-northeast-2'}),
-      tableName: 'yiguana',
-      bucketName: 'yiguana',
-    })
-    let post: Post
-
-    beforeAll(async () => {
-      post = await api.post.create({
-        data: {
-          content: 'content',
-          title: 'title',
-          category: 'news',
-          userName: 'name',
-          userPw: 'pw',
-        },
-      })
-    })
-
-    it('list(0)', async () => {
-      const {items} = await api.comment.list({
-        postId: post.hk
-      })
-      expect(items.length).toEqual(0)
-    })
-
+describe('unit', () => {
+  describe('api', () => {
     describe('comment', () => {
-      it('create comment', async () => {
+
+      const api = createApi({
+        ddbClient: new DynamoDB.DocumentClient({region: 'ap-northeast-2'}),
+        s3Client: new S3({region: 'ap-northeast-2'}),
+        tableName: 'yiguana',
+        bucketName: 'yiguana',
       })
-      it('update comment', async () => {
+      let post: Post
+
+      beforeAll(async () => {
+        post = await api.post.create({
+          data: {
+            content: 'content',
+            title: 'title',
+            category: 'news',
+            userName: 'name',
+            userPw: 'pw',
+          },
+        })
       })
-      it('like comment', async () => {
+
+      it('list(0)', async () => {
+        const {items} = await api.comment.list({
+          postId: post.hk
+        })
+        expect(items.length).toEqual(0)
       })
-      it('unlike comment', async () => {
+
+      describe('comment', () => {
+        it('create comment', async () => {
+        })
+        it('update comment', async () => {
+        })
+        it('like comment', async () => {
+        })
+        it('unlike comment', async () => {
+        })
+        it('view comment', async () => {
+        })
+        it('remove comment', async () => {
+        })
+        it('request to block comment', async () => {
+        })
       })
-      it('view comment', async () => {
-      })
-      it('remove comment', async () => {
-      })
-      it('request to block comment', async () => {
-      })
+
     })
   })
 })
