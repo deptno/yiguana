4e4f1cc98e83c89a2ac82887d7bd1e5befa57695
jhgu <jhgu.dev@gmail.com>
2019-10-21 21:24:39 +0900

    fix bug: has no exported member 'PostNonMemberInput'

--- src/api/comment/update.ts
+++ src/api/comment/update.ts
@@ -1,9 +1,7 @@
 import {YiguanaStore} from '../../store/dynamodb/dynamodb'
-import {Post, PostNonMemberInput, PostUserInput} from '../../entity/post'
+import {Post, PostUserInput} from '../../entity/post'
 import {EntityFactory} from '../../entity'
 import {User} from '../../entity/user'
-import {EValidationErrorMessage, ValidationError} from '../../entity/error'
-import * as R from 'ramda'
 
 export async function update(store: YiguanaStore<Post>, ep: EntityFactory, input: Input) {
   console.error('todo')
