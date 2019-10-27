5d9e40fb496942255909ab99cbbc6064c95a5a6e
bglee <deptno@gmail.com>
2019-10-27 17:16:12 +0900

    feat: e2e ui 에 비회원 정보 및 타이틀 입력 창 생성

--- __tests__/e2e/src/components/board/Editor.tsx
+++ __tests__/e2e/src/components/board/Editor.tsx
@@ -1,5 +1,4 @@
 import React, {FunctionComponent, useEffect, useRef, useState} from 'react'
-import Head from 'next/head'
 import {LineButton} from './LineButton'
 import * as Q from 'quill'
 
@@ -28,7 +27,53 @@ export const Editor: FunctionComponent<Props> = props => {
   }, [ref])
 
   return (
-    <div className="pv3">
+    <div className="mv3">
+      <form className="black-80">
+        <h3>비회원</h3>
+        <div className="flex justify-between">
+          <div className="w4">
+            <div className="flex flex-column items-stretch justify-between">
+              <label htmlFor="title" className="f6 b db mb2">
+                카테고리
+              </label>
+              <div className="pa2 mb2">
+                <select name="category">
+                  <option value="news">채널등록요청</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="flex-auto mh2">
+            <label htmlFor="name" className="f6 b db mb2">이름</label>
+            <input
+              id="name"
+              className="input-reset ba b--black-20 pa2 mb2 db w-100"
+              type="text"
+              aria-describedby="name"
+            />
+          </div>
+          <div className="flex-auto mh2">
+            <label htmlFor="password" className="f6 b db mb2">비밀번호</label>
+            <input
+              id="password"
+              className="input-reset ba b--black-20 pa2 mb2 db w-100"
+              type="password"
+              aria-describedby="password"
+            />
+          </div>
+        </div>
+
+        <div className="flex-auto">
+          <label htmlFor="title" className="f6 b db mb2">제목</label>
+          <input
+            id="title"
+            className="input-reset ba b--black-20 pa2 mb2 db w-100"
+            type="text"
+            aria-describedby="title"
+          />
+        </div>
+      </form>
+
       <div ref={ref}/>
       <LineButton onClick={save}>저장하기</LineButton>
     </div>
