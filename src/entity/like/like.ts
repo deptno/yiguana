f20d25777c018eaa60131fa53684513483aa9501
bglee <deptno@gmail.com>
2019-10-29 00:50:54 +0900

    refactor: 유효성 체크 밖으로 이동

--- src/entity/like/like.ts
+++ src/entity/like/like.ts
@@ -1,19 +1,11 @@
 import {YiguanaDocument} from '../../dynamodb/yiguana-document'
-import {User} from '../user'
+import {Member} from '../user'
 import {uuid} from '../../lib/uuid'
 import {LikeInput} from './user-input'
 import {EEntity} from '../enum'
 
 export function createLike(params: CreateLikeInput): Like {
   const {user, data} = params
-
-  if (!user) {
-    throw new Error('user is required')
-  }
-  if (!('userId' in user)) {
-    throw new Error('user.userId is required')
-  }
-
   const {entity, targetId, createdAt} = data
   const like: Like = {
     hk: uuid(),
@@ -26,7 +18,7 @@ export function createLike(params: CreateLikeInput): Like {
 
 export type CreateLikeInput = {
   data: LikeInput
-  user: User
+  user: Member
 }
 export interface Like extends YiguanaDocument {
   rk: EEntity.Like
