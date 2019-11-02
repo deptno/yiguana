6719ff92257527ab9f8c9053b5e589fa704450ec
deptno <deptno@gmail.com>
2019-11-02 14:55:29 +0900

    feat: 회원인 경우 포스트에 자신이 작성자인지 확인하는 로직 추가

--- __tests__/e2e/src/components/post/Post.tsx
+++ __tests__/e2e/src/components/post/Post.tsx
@@ -1,17 +1,26 @@
-import React, {FunctionComponent} from 'react'
+import React, {FunctionComponent, useContext, useMemo} from 'react'
 import locale from 'date-fns/locale/ko'
 import {formatDistanceToNow, parseISO} from 'date-fns'
 import {Post as TPost} from '../../../../../src/entity/post'
 import * as R from 'ramda'
+import {StorageContext} from '../../context/StorageContext'
 
 export const Post: FunctionComponent<Props> = props => {
   const {data, setPost} = props
-
   if (!data) {
     return null
   }
-
+  const {user} = useContext(StorageContext)
   const {hk, rk, title, contentUrl, userId, createdAt, updatedAt, children, views, likes, order} = data
+  const isAuthor = useMemo(() => {
+    if (user) {
+      if ('id' in user) {
+        return user.id === userId
+      }
+    }
+    return false
+  }, [user])
+
   const like = () => {
     fetch(`/api/post/${hk}/like`, {method: 'post'})
       .then<TPost>(response => response.json())
@@ -34,11 +43,11 @@ export const Post: FunctionComponent<Props> = props => {
       </header>
       <div className="ph3 lh-copy flex bg-light-gray hover-bg-light-pink">
           <span>
-           작성자: {userId}
+           작성자: {userId} (유저 이름 || 유저 아이디)
           </span>
       </div>
       <div className="ph3 lh-copy bg-gray white tr">
-        작성자 메뉴 공간, 수정, 삭제 등
+        작성자 메뉴 공간, 수정, 삭제 등 {isAuthor ? '작성자' : '권한없음'}
       </div>
       <pre className="pa3 pre-wrap overflow-x-scroll f7 bg-black-10 ba b--dashed">
         {JSON.stringify(data, null, 2)}
@@ -47,11 +56,11 @@ export const Post: FunctionComponent<Props> = props => {
         {data.content}
       </pre>
       <div className="justify-center mv3 lh-copy flex">
-        <a className="pa2 link near-black dib white bg-hot-pink mh2 nowrap" onClick={like}>
+        <a className="pa2 link near-black dib white bg-hot-pink mh2 nowrap pointer hover-bg-blue" onClick={like}>
           <i className="far fa-thumbs-up"/>
           <span className="ml2"> {likes}</span>
         </a>
-        <a className="pa2 link near-black dib white bg-hot-pink mh2 nowrap w-auto" href="#">
+        <a className="pa2 link near-black dib white bg-hot-pink mh2 nowrap w-auto pointer hover-bg-blue">
           <i className="far fa-bookmark"/>
           <span className="dn di-ns ml2">북마크</span>
         </a>
