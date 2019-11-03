fca9cb74a2e57199850297d35eddf9d35ab51d0b
jhgu <jhgu.dev@gmail.com>
2019-11-04 00:34:43 +0900

    repliesByUserId 구현 (#37)

---
+++ src/store/dynamodb/replies-by-user-id.ts
@@ -0,0 +1,33 @@
+import {DynamoDBInput} from '../../entity/input/dynamodb'
+import {EEntity} from '../../entity/enum'
+import {EIndexName} from '../../dynamodb/yiguana-index'
+import {Reply} from '../../entity/reply'
+
+export function repliesByUserId<T = Reply>(operator: DynamoDBInput, params: RepliesByUserIdInput) {
+  const {tableName, dynamodb} = operator
+  const {userId, exclusiveStartKey} = params
+
+  const queryParams = {
+    TableName: tableName,
+    IndexName: EIndexName.ByUser,
+    KeyConditionExpression: '#h = :h AND begins_with(#r, :r)',
+    ExpressionAttributeNames: {
+      '#h': 'userId',
+      '#r': 'order',
+    },
+    ExpressionAttributeValues: {
+      ':h': userId,
+      ':r': EEntity.Reply
+    },
+    ScanIndexForward: false,
+    ReturnConsumedCapacity: 'TOTAL',
+    ExclusiveStartKey: exclusiveStartKey
+  }
+  return dynamodb.query<T>(queryParams)
+}
+
+export type RepliesByUserIdInput = {
+  userId: string
+  postId?: string
+  exclusiveStartKey?: Exclude<any, string | number>
+}
