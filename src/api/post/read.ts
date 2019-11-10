c4e6131e11e05252981b90a9647942dd1914fca9
bglee <deptno@gmail.com>
2019-11-10 20:50:13 +0900

    fix: post.read -> comment.read 를 바라보고 있떤 오류 수정

--- src/api/post/read.ts
+++ src/api/post/read.ts
@@ -3,9 +3,7 @@ import {EntityFactory} from '../../entity'
 import {YiguanaDocumentHash} from '../../dynamodb/yiguana-document'
 
 export async function read(store: MetadataStore, ep: EntityFactory, input: ReadInput) {
-  return store.comment({
-    hk: input.data.hk
-  })
+  return store.post(input.data)
 }
 
 export type ReadInput = {
