f396e9ecbf83c463fe7960a64414fc379ebc631e
bglee <deptno@gmail.com>
2019-11-11 01:54:10 +0900

    fix: test case 가 존재 하지 않아 나는 에러에 it.todo 추가

--- __tests__/unit/store/dynamodb/report.spec.ts
+++ __tests__/unit/store/dynamodb/report.spec.ts
@@ -5,6 +5,7 @@ describe('unit', function () {
     describe('dynamodb', function () {
       describe('report', function () {
         // TODO: like.spec.ts 로 부터 복사해서 시작
+        it.todo('에러 방지용')
       })
     })
   })
