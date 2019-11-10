5b11def0157bbd843d096dfb11ea7e9aa46dc7f3
bglee <deptno@gmail.com>
2019-11-10 20:55:20 +0900

    fix: 삭제된 interface 참조 테스트 코드 삭제

--- __tests__/unit/api/reply.spec.ts
+++ __tests__/unit/api/reply.spec.ts
@@ -72,34 +72,6 @@ describe('unit', () => {
         expect(found.content).toEqual(content)
         expect(found).toEqual(updatedReply)
       })
-      xit('like reply', async() => {
-        const {items: before} = await api.reply.list({comment})
-        expect(before[0]).not.toEqual(undefined)
-
-        await api.reply.like({
-          data: before[0],
-          user: member_a,
-        })
-        const {items: after} = await api.reply.list({comment})
-        expect(after[0].likes).toEqual(before[0].likes + 1)
-      })
-      xit('like comment + like comment => cancel like reply', async () => {
-        const {items: first} = await api.reply.list({comment})
-        await api.reply.like({
-          data: first[0],
-          user: member_b,
-        })
-        const {items: second} = await api.reply.list({comment})
-        expect(second[0].likes).toEqual(first[0].likes + 1)
-
-        console.debug('like post 1회 수행하여 like가 이미 존재할 시 unlike 동작')
-        await api.reply.like({
-          data: second[0],
-          user: member_b,
-        })
-        const {items: third} = await api.reply.list({comment})
-        expect(third[0].likes).toEqual(second[0].likes - 1)
-      })
 
       it('remove reply', async() => {
         const {items: replyItems} = await api.reply.list({comment})
