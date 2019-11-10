bcca992a084f79c7779b0d966eb23089b88eba03
bglee <deptno@gmail.com>
2019-11-11 01:57:33 +0900

    fix: 에러 수정

--- src/api/post/read.ts
+++ src/api/post/read.ts
@@ -3,7 +3,7 @@ import {EntityFactory} from '../../entity'
 import {YiguanaDocumentHash} from '../../dynamodb'
 
 export async function read(store: MetadataStore, ep: EntityFactory, input: ReadInput) {
-  return store.comment(input.data)
+  return store.post(input.data)
 }
 
 export type ReadInput = {
