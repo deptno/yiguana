998b6bfef93753f75cf42e6a60779e860663bda8
bglee <deptno@gmail.com>
2019-11-22 23:24:43 +0900

    feat: backend deploy 준비

---
+++ example/web/backend/handler/graphql.ts
@@ -0,0 +1,19 @@
+import {ApolloServer, makeExecutableSchema} from 'apollo-server-lambda'
+import {typeDefs} from '../../frontend/src/pages/api/graphql/typeDefs'
+import {resolvers} from '../../frontend/src/pages/api/graphql/resolvers'
+import {context} from '../../frontend/src/pages/api/graphql/context'
+import {dataSources} from '../../frontend/src/pages/api/graphql/dataSource'
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
+const apolloServer = new ApolloServer({
+  schema,
+  context,
+  dataSources,
+  playground: true,
+  introspection: true,
+})
+
+export default apolloServer.createHandler()
--- example/web/frontend/src/pages/api/lib/token.ts
+++ example/web/frontend/src/pages/api/lib/token.ts
@@ -2,4 +2,3 @@ import {scryptSync} from 'crypto'
 
 const DDB_ENCRYPT_KEY = 'dummy dynamodb encrypt key'
 export const SALT = scryptSync(DDB_ENCRYPT_KEY!, 'salt', 24)
-
