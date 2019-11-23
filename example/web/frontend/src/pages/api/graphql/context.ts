36c3e28707c3ab918f3b0ecfbd01cb0f0eebe281
bglee <deptno@gmail.com>
2019-11-23 19:31:14 +0900

    feat: implement context for multiple env that are next.js | lambda

--- example/web/frontend/src/pages/api/graphql/context.ts
+++ example/web/frontend/src/pages/api/graphql/context.ts
@@ -1,20 +1,37 @@
 import {Member} from '../../../../../../../src/entity/user'
 
 export const context = ({req, event}) => {
-  if (req) {
-    // next.js
-    if (req.headers.authorization) {
-      return {
-        user: JSON.parse(req.headers.authorization) as Member
+  if (event) {
+    return lambda(event)
+  }
+  return nextjs(req)
+}
+
+const lambda = event => {
+  const {headers, requestContext: {identity: {sourceIp: ip}}} = event
+  const {Authorization, authorization = Authorization} = headers
+
+  if (authorization) {
+    console.log('authorization', authorization)
+    const user = JSON.parse(authorization) as Member
+    console.log('user', user)
+    return {
+      user: {
+        ...user,
+        ip
       }
     }
   }
-  if (event) {
-    // lambda
-    if (event.headers.authorization) {
-      return {
-        user: req.headers.authorization as Member
-      }
+}
+const nextjs = req => {
+  console.log(req)
+  const {Authorization, authorization = Authorization} = req.headers
+
+  if (authorization) {
+    const user = JSON.parse(req.headers.authorization) as Member
+
+    return {
+      user,
     }
   }
 }
