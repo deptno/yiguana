b5b467de39d636e26a438a8e633f33f40d61cbe9
bglee <deptno@gmail.com>
2019-11-24 23:18:52 +0900

    feat: 마이페이지 레이아웃 변경

--- example/web/frontend/src/pages/my-page/index.tsx
+++ example/web/frontend/src/pages/my-page/index.tsx
@@ -10,31 +10,34 @@ import {UserReportedComments} from '../../components/board/UserReportedComments'
 const MyPage: NextPage<Props> = props => {
   return (
     <div className="pa3 flex-column">
-      <div>
-        내가 공감 표시한 아이템들
-        <UserLikePosts />
-      </div>
-      <div>
-        내가 공감 표시한 코멘트
-        <UserLikeComments/>
-      </div>
-      <hr/>
-      <div>
-        내가 작성한 글들
-        <UserPosts/>
-      </div>
-      <div>
-        내가 작성한 코멘트
-        <UserComments/>
+      내가 공감한
+      <div className="flex-l">
+        <div className="w-100">
+          <UserLikePosts/>
+        </div>
+        <div className="w-100">
+          <UserLikeComments/>
+        </div>
       </div>
       <hr/>
-      <div>
-        내가 신고한 글
-        <UserReportedPosts/>
+      내가 작성한
+      <div className="flex-l">
+        <div className="w-100">
+          <UserPosts/>
+        </div>
+        <div className="w-100">
+          <UserComments/>
+        </div>
       </div>
-      <div>
-        내가 신고한 코멘트
-        <UserReportedComments/>
+      <hr/>
+      내가 신고한
+      <div className="flex-l">
+        <div className="w-100">
+          <UserReportedPosts/>
+        </div>
+        <div className="w-100">
+          <UserReportedComments/>
+        </div>
       </div>
     </div>
   )
