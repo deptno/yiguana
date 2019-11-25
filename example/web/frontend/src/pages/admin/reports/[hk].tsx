2fd3eb366fb643603ce2d6111912aa7d65840406
bglee <deptno@gmail.com>
2019-11-25 21:27:43 +0900

    feat: 신고 상세페이지까지 구현

--- example/web/frontend/src/pages/admin/reports/[hk].tsx
+++ example/web/frontend/src/pages/admin/reports/[hk].tsx
@@ -3,21 +3,40 @@ import {NextPage} from 'next'
 import {useLazyQuery} from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import {useRouter} from 'next/router'
+import Link from 'next/link'
+import {BoardItem} from '../../../components/board/BoardItem'
 
 const ReportsPage: NextPage<Props> = props => {
   const {query: {hk}} = useRouter()
   const [fetch, {data, error}] = useLazyQuery(gql`
     query ($hk: String!, $rk: String!) {
+      post(hk: $hk) {
+        hk
+        rk
+        title
+        likes
+        views
+        children
+        category
+        createdAt
+        userId
+        cover
+
+        dCategory
+        deleted
+      }
       reports(hk: $hk, rk: $rk) {
         items {
           hk
+          userId
+          content
+
         }
         firstResult
         cursor
       }
     }
   `)
-
 
   useEffect(() => {
     if (hk) {
@@ -29,10 +48,31 @@ const ReportsPage: NextPage<Props> = props => {
       })
     }
   }, [hk])
+
   console.log(hk, data, error)
+
   return (
     <div className="pa3 flex-column">
-      agg report
+      <div className="flex-auto">
+        {data?.post && <BoardItem item={data?.post} no={0} onDelete={console.log}/>}
+      </div>
+      신고받은 게시물들
+      {data?.reports?.items.map((r, no) => {
+        return (
+          <Link key={`${r.hk}#${r.userId}`} href="/admin/reports/[hk]" as={`/admin/reports/${r.hk}`}>
+            <div className="flex flex-column pt1 ph2 pb2 ba">
+              <div className="lh-copy flex pointer mv0 pv0 lh-copy nowrap">
+                <span className="w5 dn db-ns">
+                  신고자: <i className="far fa-user-circle bg-black white pa1 br2"/> {r.userId}</span>
+                <span className="w-100 dn db-ns">신고내용: {r.content}</span>
+              </div>
+              <pre className="debug pa3 pre-wrap overflow-x-scroll f7 bg-black-10 ba b--dashed">
+              {JSON.stringify(r, null, 2)}
+            </pre>
+            </div>
+          </Link>
+        )
+      })}
     </div>
   )
 }
