422d2b8cc3fcf7a1b1ddf87dce7675e5779d4325
bglee <deptno@gmail.com>
2019-11-29 00:05:09 +0900

    feat: 댓글확인에 새로고침 연결 #105

--- example/web/frontend/src/components/post/Comments.tsx
+++ example/web/frontend/src/components/post/Comments.tsx
@@ -97,7 +97,7 @@ export const Comments: FunctionComponent<Props> = props => {
   return (
     <>
       <div className="mh2 mv3 flex flex-column">
-        <span className="mb2">
+        <span className="mb2 pointer" onClick={() => refetch()}>
           <i className="mh2 far fa-comment-dots"/>댓글 ({Math.max(items.length || count || 0)})
           <small className="mh2">
             <i className="fas fa-sync-alt"/> 새 댓글 확인
