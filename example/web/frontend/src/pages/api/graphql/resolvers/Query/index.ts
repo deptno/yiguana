612adf39aab4af20d84077968a19f3752337a0b9
bglee <deptno@gmail.com>
2019-12-02 04:33:06 +0900

    fix: 지워진 파일 임포트 에러 수정

--- example/web/frontend/src/pages/api/graphql/resolvers/Query/index.ts
+++ example/web/frontend/src/pages/api/graphql/resolvers/Query/index.ts
@@ -8,8 +8,6 @@ export * from './reports'
 
 export * from './myPosts'
 export * from './myComments'
-export * from './myReportedPosts'
-export * from './myReportedComments'
 export * from './myPostReports'
 export * from './myCommentReports'
 
